2b3c50c75eebd395d21a559f2e342bfa4b277cdf
Dimasick20009 <143876120+Dimasick20009@users.noreply.github.com>
2023-09-16 18:16:53 +0300

    Add files via upload

--- Cheats.py
+++ Cheats.py
@@ -15,5 +15,20 @@ window.image = PhotoImage(file='logo.png')
 bg_logo = Label(window, image=window.image)
 bg_logo.grid(row=0,column=0)
 
+tab_control =  ttk.Notebook(window)
+
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab_control.add(tab1, text='AIM')
+tab_control.add(tab2, text='ESP')
+
+lb1 = Label(tab1, text='AIM')
+lb1.grid(column=0, row=0)
+
+lb2 = Label(tab2, text='ESP')
+lb2.grid(column=0, row=0)
+
+tab_control.pack(expand=1, fill='both')
+
 window.mainloop()
 
