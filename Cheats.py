90068282422cba523ff116b9c6a9060a28110ef8
Dimasick20009 <143876120+Dimasick20009@users.noreply.github.com>
2023-09-17 11:05:36 +0300

    Add files via upload

--- Cheats.py
+++ Cheats.py
@@ -1,34 +1,29 @@
 from tkinter import *
 from tkinter import ttk
 
-window = Tk()
-window.title("CandyCheats")
-
-width = 400
-height = 300
-x = 200
-y = 300
-window.geometry(f"{width}x{height}+{x}+{y}")
-window.resizable(False,False)
-window.iconbitmap("icon.ico")
-window.image = PhotoImage(file='logo.png')
-bg_logo = Label(window, image=window.image)
+root = Tk()
+root.title('CandyCheats')
+root.geometry('400x250')
+root.resizable(width=False, height=False)
+root.iconbitmap("icon.ico")
+root.image = PhotoImage(file='logo.png')
+bg_logo = Label(root, image=root.image)
 bg_logo.grid(row=0,column=0)
 
-tab_control =  ttk.Notebook(window)
+tab_control = ttk.Notebook(root)
 
 tab1 = ttk.Frame(tab_control)
 tab2 = ttk.Frame(tab_control)
-tab_control.add(tab1, text='AIM')
-tab_control.add(tab2, text='ESP')
+tab_control.add(tab1, text='ESP')
+tab_control.add(tab2, text='AIM')
 
-lb1 = Label(tab1, text='AIM')
+lb1 = Label(tab1, text='ESP')
 lb1.grid(column=0, row=0)
 
-lb2 = Label(tab2, text='ESP')
+lb2 = Label(tab1, text='ESP')
 lb2.grid(column=0, row=0)
 
 tab_control.pack(expand=1, fill='both')
+root.mainloop()
 
-window.mainloop()
 
